612bf8a81ac46aa245b957f7aa702ad718ae5c2f
mianumar1111 <umarmian447@gmail.com>
2024-05-28 15:15:19 +0500

    Add client folder

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import "./form.css";
+import axios from "axios";
+import { BsFillTrash3Fill } from "react-icons/bs";
+
+const Form = () => {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const result = await axios.get("http://localhost:5000/get");
+      setTodos(result.data);
+    };
+
+    fetchTodos();
+  }, [task]);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (task === "") {
+      alert("Please fill the task");
+    } else {
+      axios.post("http://localhost:5000/add", { task: task });
+      setTask("");
+    }
+  };
+
+  const handleDone = async (id) => {
+    const result = await axios.patch(`http://localhost:5000/${id}`);
+    setTodos(result.data);
+  };
+
+  const handleUnDone = async (id) => {
+    const result = await axios.patch(`http://localhost:5000/undone/${id}`);
+    setTodos(result.data);
+  };
+
+  const handleDel = async (id) => {
+    const result = await axios.delete(`http://localhost:5000/${id}`);
+    setTodos(result.data);
+  };
+
+  return (
+    <div className="main">
+      <div>
+        <h1>TO-DO-LIST</h1>
+      </div>
+
+      <form className="form" onSubmit={handleAdd}>
+        <input
+          id="inputtask"
+          type="text"
+          placeholder="Enter your task here"
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
+        />
+        <input id="addbutton" value="Add Task" type="submit" />
+      </form>
+      <div className="list">
+        {todos.length === 0 ? (
+          <div>
+            <h2>No Task Available</h2>
+          </div>
+        ) : (
+          todos.map((todo, index) => (
+            <div className="task" key={index}>
+              <div>
+                {todo.status === true ? (
+                  <div className="listItem">
+                    <p
+                      onClick={() => {
+                        handleUnDone(todo._id);
+                      }}
+                      className="done"
+                      >
+                    
+                      {todo.task}
+                    </p>
+                    <p
+                      onClick={() => {
+                        handleDel(todo._id);
+                      }}
+                      className="del"
+                    >
+                      <BsFillTrash3Fill />
+                    </p>
+                  </div>
+                ) : (
+                  <div className="listItem">
+                    <p
+                      onClick={() => {
+                        handleDone(todo._id);
+                      }}
+                    >
+                      {todo.task}
+                    </p>
+                    <p
+                      onClick={() => {
+                        handleDel(todo._id);
+                      }}
+                      className="del"
+                    >
+                      <BsFillTrash3Fill />
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Form;
