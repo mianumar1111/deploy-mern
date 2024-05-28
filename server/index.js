3d468952f0029aa9af62e9bc3c3494a61576eeae
mianumar1111 <umarmian447@gmail.com>
2024-05-28 15:50:15 +0500

    changes

--- server/index.js
+++ server/index.js
@@ -24,9 +24,6 @@ mongoose
 
 const port = 5000;
 
-app.post("/", (req, res) => {
-  res.send('Hello')
-});
 app.post("/add", (req, res) => {
   const task = req.body.task;
   todoModel.create({ task: task }).then((result) => {
