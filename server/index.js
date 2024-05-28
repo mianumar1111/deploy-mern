1a01a88dd417af2243918f031d17f2899120e806
mianumar1111 <umarmian447@gmail.com>
2024-05-28 15:30:12 +0500

    changes in the index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 const todoModel = require("./Model/todoModel");
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://deploy-mern-frontend-eta.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
 mongoose
